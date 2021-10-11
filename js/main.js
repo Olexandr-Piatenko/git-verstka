0c5dbe57d8681f5791155ef1c70f6c43f881a5d7
Piatenko-ol <piatenko.web@gmail.com>
2021-10-11 20:56:39 +0300

    section__ reviews

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(function(){
+  $('.reviews__inner').slick({
+    dots: false,
+    
+    autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: true,
+  
+  slidesToShow: 1,
+  prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="../images/Arrow-left.svg"></button>',
+  nextArrow:'<button type="button" class="slick-btn slick-next"><img src="../images/Arrow-right.svg"></button>',
 
+  });
+
+  
 });
